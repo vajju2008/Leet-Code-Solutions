6d3725212870ec345e23b937854d5c94ebcb719d
SaiVatsal <vatsal1327@gmail.com>
2026-04-13 19:48:21 +0530

    1848-minimum-distance-to-the-target-element .py Easy 04-13-2026

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        min_distance = float('inf')
+
+        for i, num in enumerate(nums):
+            if num == target:
+                min_distance = min(min_distance,abs(i - start))
+
+        return min_distance
+                
+
+
