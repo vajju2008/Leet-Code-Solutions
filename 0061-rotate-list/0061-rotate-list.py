5ea741b730c712ea33f8d12f99ec1547f3805e1f
SaiVatsal <vatsal1327@gmail.com>
2026-05-05 20:08:25 +0530

    0061-rotate-list .py Medium 05-05-2026

---
+++ 0061-rotate-list/0061-rotate-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) 
+        if not head or not head.next or k == 0: return head 
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k %= length
+        if k == 0: return head ##
+
+        tail.next = head
+
+        steps = length - k
+        newtail = head
+        for _ in  range(1, steps):
+            newtail = newtail.next
+        
